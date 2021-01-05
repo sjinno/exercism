9c35bdf9c9eec31d0d9588be2ab434bdea7d66db
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-05 12:20:17 -0800

    Finish armstrong-numbers exercise.

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,8 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let num_in_string = num.to_string();
+    let length = num_in_string.len() as u32;
+    num_in_string.chars().into_iter().fold(0, |acc, x| {
+        let x = x.to_digit(10).unwrap();
+        acc + x.pow(length)
+    }) == num
 }
