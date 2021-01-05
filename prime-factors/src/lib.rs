0bf59b36f85c1f9319da8b3bc5b6d99f2e4992bb
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-05 00:42:18 -0800

    Finish prime-factors exercise.

---
+++ prime-factors/src/lib.rs
@@ -0,0 +1,23 @@
+pub fn factors(n: u64) -> Vec<u64> {
+    if n == 1 {
+        return vec![];
+    }
+    let mut n = n.to_owned();
+    let mut d = 2;
+    let mut ans = vec![];
+
+    loop {
+        if n.div_euclid(d) == 1 && n.rem_euclid(d) == 0 {
+            ans.push(d);
+            break;
+        }
+        if n.rem_euclid(d) == 0 {
+            n /= d;
+            ans.push(d);
+            continue;
+        }
+        d += 1;
+    }
+
+    ans
+}
