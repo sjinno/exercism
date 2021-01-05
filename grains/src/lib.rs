783e21ec03064c3929be74becbc96868e66806d4
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-05 00:43:41 -0800

    Finish grains exercise.

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,7 +1,10 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    match s {
+        num if num == 0 || num > 64 => panic!("Square must be between 1 and 64"),
+        _ => 2u64.pow(s - 1),
+    }
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    (0..64).into_iter().fold(0, |acc, x| acc + 2u64.pow(x))
 }
