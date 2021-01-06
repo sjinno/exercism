8e6fa9fe5c8ea96ccc7897b5678a2e584c830ac8
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-06 14:53:21 -0800

    series: Remove unnecessary if statements.

--- series/src/lib.rs
+++ series/src/lib.rs
@@ -1,12 +1,4 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    if len > 5 {
-        return vec![];
-    }
-
-    if len == 5 {
-        return vec![digits.to_string()];
-    }
-
     (0..6 - len)
         .scan(0, |_, x| {
             let num = digits.get(x..x + len).unwrap();
