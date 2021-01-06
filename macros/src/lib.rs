709df7799ec4e59f0eb5e5f7dacdbfd9920df677
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-05 16:53:02 -0800

    Add mentor solution.

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,21 +1,31 @@
+// #[macro_export]
+// macro_rules! hashmap {
+//     ( $( $key:expr => $value:expr ),* ) => {
+//             {
+//                 let mut temp_hm = ::std::collections::HashMap::new();
+//                 $(
+//                     temp_hm.insert($key, $value);
+//                 )*
+//                 temp_hm
+//             }
+//     };
+//     ( $( $key:expr => $value:expr, )* ) => {
+//         {
+//             let mut temp_hm = ::std::collections::HashMap::new();
+//             $(
+//                 temp_hm.insert($key, $value);
+//             )*
+//             temp_hm
+//         }
+//     };
+// }
+
+// Mentor solution
 #[macro_export]
 macro_rules! hashmap {
-    ( $( $key:expr => $value:expr ),* ) => {
-            {
-                let mut temp_hm = ::std::collections::HashMap::new();
-                $(
-                    temp_hm.insert($key, $value);
-                )*
-                temp_hm
-            }
-    };
-    ( $( $key:expr => $value:expr, )* ) => {
-        {
-            let mut temp_hm = ::std::collections::HashMap::new();
-            $(
-                temp_hm.insert($key, $value);
-            )*
-            temp_hm
-        }
-    };
+    ($($( $key: expr => $val: expr )+$(,)?)*) => {{
+         let mut map = ::std::collections::HashMap::new();
+         $($( map.insert($key, $val); )*)*
+         map
+    }}
 }
