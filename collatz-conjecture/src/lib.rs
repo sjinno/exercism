9a3e792e66bbd03200cdedc5941e7b1af8953fd1
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-06 15:25:21 -0800

    Dinish collatz-conjecture exercise.

--- collatz-conjecture/src/lib.rs
+++ collatz-conjecture/src/lib.rs
@@ -1,6 +1,26 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {}",
-        n,
-    )
+    if n == 0 {
+        return None;
+    }
+
+    if n == 1 {
+        return Some(0);
+    }
+
+    let mut num = n.to_owned();
+    (0..)
+        .scan(1, |s, _| {
+            if num % 2 == 0 {
+                num /= 2;
+            } else {
+                num = num * 3 + 1;
+            }
+            *s += 1;
+            if num != 1 {
+                Some(*s)
+            } else {
+                None
+            }
+        })
+        .last()
 }
