1a1e73358f1a209b84a21990354f39c9ac265a87
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-06 13:37:16 -0800

    Finish matching-brackets exercise.

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,6 +1,25 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+    let mut bracket_stack = vec![];
+    for c in string.chars().into_iter() {
+        match c {
+            c if c == '(' || c == '[' || c == '{' => {
+                bracket_stack.push(c);
+            }
+            c if c == ')' || c == ']' || c == '}' => {
+                if bracket_stack.is_empty() {
+                    return false;
+                }
+                let bracket = bracket_stack.pop().unwrap();
+                if c == ')' && bracket != '(' {
+                    return false;
+                } else if c == ']' && bracket != '[' {
+                    return false;
+                } else if c == '}' && bracket != '{' {
+                    return false;
+                }
+            }
+            _ => continue,
+        }
+    }
+    bracket_stack.is_empty()
 }
