f222131f169ff4ad565499b445ecb6359264acdd
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-06 13:51:14 -0800

    matching-brackets: Add community solution.

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,25 +1,52 @@
+// pub fn brackets_are_balanced(string: &str) -> bool {
+//     let mut bracket_stack = vec![];
+//     for c in string.chars().into_iter() {
+//         match c {
+//             c if c == '(' || c == '[' || c == '{' => {
+//                 bracket_stack.push(c);
+//             }
+//             c if c == ')' || c == ']' || c == '}' => {
+//                 if bracket_stack.is_empty() {
+//                     return false;
+//                 }
+//                 let bracket = bracket_stack.pop().unwrap();
+//                 if c == ')' && bracket != '(' {
+//                     return false;
+//                 } else if c == ']' && bracket != '[' {
+//                     return false;
+//                 } else if c == '}' && bracket != '{' {
+//                     return false;
+//                 }
+//             }
+//             _ => continue,
+//         }
+//     }
+//     bracket_stack.is_empty()
+// }
+
+// Community solution 1
 pub fn brackets_are_balanced(string: &str) -> bool {
-    let mut bracket_stack = vec![];
-    for c in string.chars().into_iter() {
+    let mut brackets: Vec<char> = Vec::new();
+    for c in string.chars() {
         match c {
-            c if c == '(' || c == '[' || c == '{' => {
-                bracket_stack.push(c);
-            }
-            c if c == ')' || c == ']' || c == '}' => {
-                if bracket_stack.is_empty() {
-                    return false;
-                }
-                let bracket = bracket_stack.pop().unwrap();
-                if c == ')' && bracket != '(' {
-                    return false;
-                } else if c == ']' && bracket != '[' {
-                    return false;
-                } else if c == '}' && bracket != '{' {
+            '(' | '{' | '[' => brackets.push(c),
+            ')' => {
+                if brackets.pop() != Some('(') {
                     return false;
                 }
             }
-            _ => continue,
+            ']' => {
+                if brackets.pop() != Some('[') {
+                    return false;
+                }
+            }
+            '}' => {
+                if brackets.pop() != Some('{') {
+                    return false;
+                }
+            }
+            _ => (),
         }
     }
-    bracket_stack.is_empty()
+    brackets.is_empty()
 }
