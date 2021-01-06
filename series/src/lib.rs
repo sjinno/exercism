45330dcb41e929d9a9274c0964e5b6c47c5366fa
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-06 14:50:36 -0800

    Finish series exercise.

--- series/src/lib.rs
+++ series/src/lib.rs
@@ -1,7 +1,16 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    unimplemented!(
-        "What are the series of length {} in string {:?}",
-        len,
-        digits
-    )
+    if len > 5 {
+        return vec![];
+    }
+
+    if len == 5 {
+        return vec![digits.to_string()];
+    }
+
+    (0..6 - len)
+        .scan(0, |_, x| {
+            let num = digits.get(x..x + len).unwrap();
+            Some(num.to_string())
+        })
+        .collect::<Vec<String>>()
 }
