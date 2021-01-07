c00683a3ee1065ada6da6a4743340124b1b1be22
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-07 12:09:21 -0800

    (WIP): dot-dsl: Write fields for Graph struct.

---
+++ dot-dsl/src/graph/graph_items.rs
@@ -0,0 +1,4 @@
+pub mod edge;
+pub mod node;
+pub use edge::Edge;
+pub use node::Node;
--- dot-dsl/src/lib.rs
+++ dot-dsl/src/lib.rs
@@ -1,9 +1,27 @@
 pub mod graph {
-    pub struct Graph;
+    // pub mod graph_items;
+    // pub use graph_items::*;
+    use crate::graph::graph_items::edge::Edge;
+    use crate::graph::graph_items::node::Node;
+
+    pub struct Graph<'a> {
+        nodes: Vec<Node>,
+        edges: Vec<Edge>,
+        attrs: &'a [&'a str],
+    }
 
     impl Graph {
         pub fn new() -> Self {
-            unimplemented!("Construct a new Graph struct.");
+            Graph
+        }
+    }
+
+    pub mod graph_items {
+        pub mod node {
+            pub struct Node;
+        }
+        pub mod edge {
+            pub struct Edge;
         }
     }
 }
