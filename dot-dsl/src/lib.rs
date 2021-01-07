a327f46a8bd02866914c0db655f5a548714d6fbe
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-07 12:55:28 -0800

    (WIP): dot-dsl: Add new() and with_nodes() methods to Graph & new() mehtod to Node.

--- dot-dsl/src/lib.rs
+++ dot-dsl/src/lib.rs
@@ -5,20 +5,38 @@ pub mod graph {
     use crate::graph::graph_items::node::Node;
 
     pub struct Graph<'a> {
-        nodes: Vec<Node>,
-        edges: Vec<Edge>,
-        attrs: &'a [&'a str],
+        pub nodes: Vec<Node<'a>>,
+        pub edges: Vec<Edge>,
+        pub attrs: &'a [&'a str],
     }
 
-    impl Graph {
+    impl<'a> Graph<'a> {
         pub fn new() -> Self {
-            Graph
+            Graph {
+                nodes: vec![],
+                edges: vec![],
+                attrs: Default::default(),
+            }
+        }
+
+        pub fn with_nodes(mut self, nodes: &'a Vec<Node<'a>>) -> Self {
+            self.nodes = nodes.to_owned();
+            self
         }
     }
 
     pub mod graph_items {
         pub mod node {
-            pub struct Node;
+            #[derive(Clone, Debug, PartialEq)]
+            pub struct Node<'a> {
+                node: &'a str,
+            }
+
+            impl<'a> Node<'a> {
+                pub fn new(node: &'a str) -> Self {
+                    Node { node: node }
+                }
+            }
         }
         pub mod edge {
             pub struct Edge;
