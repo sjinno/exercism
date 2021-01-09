f2dce21b76138d84b3de4788a82d3d4f82a04433
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-08 19:28:23 -0800

    pascals-triangle: Done.

--- pascals-triangle/src/lib.rs
+++ pascals-triangle/src/lib.rs
@@ -1,11 +1,28 @@
-pub struct PascalsTriangle;
+pub struct PascalsTriangle {
+    pub count: u32,
+}
 
 impl PascalsTriangle {
     pub fn new(row_count: u32) -> Self {
-        unimplemented!("create Pascal's triangle with {} rows", row_count);
+        PascalsTriangle { count: row_count }
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
-        unimplemented!();
+        let mut rows: Vec<Vec<u32>> = vec![];
+        for r in 0..self.count {
+            let mut row = vec![];
+
+            for c in 0..r + 1 {
+                if c == 0 || c == r {
+                    row.push(1);
+                } else {
+                    let elt =
+                        rows[r as usize - 1][c as usize - 1] + rows[r as usize - 1][c as usize];
+                    row.push(elt);
+                }
+            }
+            rows.push(row);
+        }
+        rows
     }
 }
