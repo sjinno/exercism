86a9bc5b0acb33a57a2196bbf11ef0cdeb6a6361
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-16 15:10:13 -0800

    〆 acronym: Done.

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -1,3 +1,45 @@
 pub fn abbreviate(phrase: &str) -> String {
-    unimplemented!("Given the phrase '{}', return its acronym", phrase);
+    const ALPHABET: &[char; 26] = &[
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    ];
+
+    if phrase.is_empty() {
+        return String::new();
+    }
+
+    let mut split_phrase = phrase.split(' ');
+    let mut ans = String::new();
+
+    while let Some(word) = split_phrase.next() {
+        if word.chars().into_iter().all(|c| !c.is_alphabetic()) {
+            continue;
+        }
+
+        let tmp = word.clone().to_ascii_uppercase();
+        if tmp == word {
+            ans.push(word.chars().next().unwrap());
+        } else {
+            let mut iter = word.chars();
+            while let Some(initial) = iter.next() {
+                // println!("{:?}", initial);
+                if initial.is_alphabetic() {
+                    ans.push(initial.to_ascii_uppercase());
+                    break;
+                }
+            }
+            while let Some(c) = iter.next() {
+                if ALPHABET.contains(&c) {
+                    ans.push(c);
+                } else if c == '-' {
+                    if let Some(ch) = iter.next() {
+                        // println!("{:?}", ch);
+                        ans.push(ch.to_ascii_uppercase());
+                    }
+                }
+            }
+        }
+    }
+
+    ans
 }
