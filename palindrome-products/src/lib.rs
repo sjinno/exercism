8efe7b72ac39cfad3f9125ae8619f8fad7762bf5
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-16 09:52:38 -0800

    (WIP) palindrome-products: Tiring...

--- palindrome-products/src/lib.rs
+++ palindrome-products/src/lib.rs
@@ -1,6 +1,7 @@
 #[derive(Debug, PartialEq, Eq)]
 pub struct Palindrome {
-    // implement your palindrome type here
+    smallest: Vec<(usize, usize)>,
+    largest: Vec<(usize, usize)>,
 }
 
 impl Palindrome {
@@ -18,9 +19,49 @@ impl Palindrome {
 }
 
 pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
-    unimplemented!(
-        "Find the min and max palindromic numbers which are products of numbers in the inclusive range ({}..{})",
-        min,
-        max
-    )
+    let smallest = std::f64::INFINITY;
+    let mut smallest_factors = Vec::<(u64, u64)>::new();
+    for a in min..=max {
+        for b in min..=max {
+            let ab = a as f64 * b as f64;
+            let pal = ab.clone().to_string().chars().collect::<Vec<char>>();
+            let reversed = pal.clone().into_iter().rev().collect::<Vec<char>>();
+            if pal == reversed {
+                if ab < smallest {
+                    smallest_factors.push((a, b));
+                    break;
+                }
+            }
+        }
+        if !smallest_factors.is_empty() {
+            break;
+        }
+    }
+
+    let mut largest = 0;
+    let mut largest_factors = vec![];
+    for a in (min..=max).rev() {
+        for b in (min..=max).rev() {
+            let ab = a * b;
+            let pal = ab.to_string().chars().collect::<Vec<char>>();
+            let reversed = pal.clone().into_iter().rev().collect::<Vec<char>>();
+            if pal == reversed {
+                if ab > largest {
+                    largest = ab;
+                    largest_factors.clear();
+                    largest_factors.push((a, b));
+                } else if ab == largest {
+                    largest_factors.push((a, b));
+                }
+            }
+            if largest_factors.len() == 2 {
+                break;
+            }
+        }
+        if largest_factors.len() == 2 {
+            break;
+        }
+    }
+
+    None
 }
