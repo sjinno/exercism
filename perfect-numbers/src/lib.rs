5c8ff997f9f6e7893c7370893a1a2df24898a0b7
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-24 13:02:56 -0800

    perfect-numbers: Done.

--- perfect-numbers/src/lib.rs
+++ perfect-numbers/src/lib.rs
@@ -5,6 +5,30 @@ pub enum Classification {
     Deficient,
 }
 
+#[rustfmt::skip]
 pub fn classify(num: u64) -> Option<Classification> {
-    unimplemented!("classify {}", num);
+    use std::cmp::Ordering::*;
+
+    match num {
+        0 => None,
+        1 => Some(Classification::Deficient),
+        _ => {
+            let total;
+            if num % 2 == 0 {
+                total = (1..=num / 2)
+                    .filter(|x| num.rem_euclid(*x) == 0)
+                    .sum::<u64>();
+            } else {
+                total = (1..=num / 3)
+                    .filter(|x| num.rem_euclid(*x) == 0)
+                    .sum::<u64>();
+            }
+
+            match total.cmp(&num) {
+                Greater => return Some(Classification::Abundant),
+                Equal   => return Some(Classification::Perfect),
+                Less    => return Some(Classification::Deficient),
+            }
+        }
+    }
 }
