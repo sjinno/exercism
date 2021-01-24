5d50d1fdc250c8b2db2847f6ee7c3c6dce276893
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-24 00:15:45 -0800

    binary-search: Done.

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,7 +1,31 @@
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return its index.",
-        key,
-        array
-    );
+    if array.is_empty() {
+        return None;
+    }
+
+    let (mut low, mut up) = (0, array.len() - 1);
+
+    while low < up {
+        let mid = (low + up) / 2;
+        let num = array[mid];
+
+        if num == key {
+            return Some(mid);
+        }
+
+        if num < key {
+            low = mid + 1;
+        } else if num > key {
+            if mid == 0 {
+                break;
+            }
+            up = mid - 1;
+        }
+    }
+
+    if array[low] == key {
+        return Some(low);
+    }
+
+    None
 }
