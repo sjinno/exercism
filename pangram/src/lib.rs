7ad129fd86365583b086caa6005a962c498fb0b8
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-30 07:49:31 -0800

    Pangram: Revise.

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -2,9 +2,9 @@ use std::collections::HashSet;
 
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    let mut hs = HashSet::new();
-    sentence.chars().into_iter().for_each(|c| { 
-        if c.is_alphabetic() { hs.insert(c); }
-    });
-    hs.len() > 25
+    sentence.chars()
+            .filter(|c| c.is_ascii_alphabetic())
+            .map(|c| c.to_ascii_lowercase())
+            .collect::<HashSet<_>>()
+            .len() == 26
 }
