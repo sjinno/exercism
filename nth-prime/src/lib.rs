392ba48acc4eb8b15372927b93cbe5c365e6ddb0
Shohei Jinno <shohei.jinno@gmail.com>
2021-02-24 15:10:27 -0800

    Use faster algorithm to check primality.

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,54 +1,48 @@
-// // Without using a library...
-// pub fn nth(n: u32) -> u32 {
-//     let mut primes = vec![2, 3, 5, 7];
-//     if n == 0 || n == 1 || n == 2 || n == 3 {
-//         return primes[n as usize];
-//     }
-
-//     let mut count = 3;
-//     let mut potential = 11;
-//     while count != n {
-//         let length = primes.len();
-//         for i in 0..length {
-//             let p = primes[i];
-//             let rem = potential % p;
-//             println!("p: {}. rem: {}.", p, rem);
-//             if rem == 0 {
-//                 potential += 2;
-//                 break;
-//             }
-
-//             if i == length - 1 {
-//                 primes.push(potential);
-//                 count += 1;
-//                 potential += 2;
-//             }
-//         }
-//     }
-
-//     return primes[n as usize];
-// }
-
-// use concurrent_prime_sieve::collection;
-
-// pub fn nth(n: u32) -> u32 {
-//     if n > 1000 {
-//         let primes = collection::primes_concurrently(n as usize * 11, 8);
-//         return primes[n as usize] as u32;
-//     }
-//     let primes = collection::primes(n as usize * 4);
-//     primes[n as usize] as u32
-// }
-
-use std::ops::RangeFrom;
-
-pub fn nth(n: u32) -> Option<u32> {
-    (RangeFrom { start: 2 })
-        .filter(|x| is_prime(x))
-        .take(n as usize)
-        .last()
+pub fn nth(n: usize) -> Option<usize> {
+    match n {
+        0 => None,
+        1 => Some(2),
+        _ => (3..).step_by(2).filter(|x| is_prime(x)).nth(n - 2),
+    }
 }
 
-fn is_prime(n: &u32) -> bool {
-    !(2..*n).any(|x| n % x == 0)
+fn is_prime(x: &usize) -> bool {
+    match x {
+        _ if x < &2 => false,
+        _ => {
+            let mut i = 2;
+            while &(i * i) <= x {
+                if x % i == 0 {
+                    return false;
+                }
+                i += 1;
+            }
+            true
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_second_prime() {
+        assert_eq!(nth(2), Some(3));
+    }
+
+    #[test]
+    fn test_sixth_prime() {
+        assert_eq!(nth(6), Some(13));
+    }
+
+    #[test]
+    fn test_big_prime() {
+        assert_eq!(nth(10001), Some(104743));
+    }
+
+    #[test]
+    fn test_zeroth_prime() {
+        assert_eq!(nth(0), None);
+    }
 }
