85c3031b876861d3432f36dd36db407353a5511e
Shohei Jinno <shohei.jinno@gmail.com>
2021-03-05 21:34:37 -0800

    Finish accumulate exercise.

--- accumulate/src/lib.rs
+++ accumulate/src/lib.rs
@@ -1,4 +1,19 @@
-/// What should the type of _function be?
-pub fn map(input: Vec<i32>, _function: ???) -> Vec<i32> {
-    unimplemented!("Transform input vector {:?} using passed function", input);
+// pub fn map<F, In, Out>(input: Vec<In>, function: F) -> Vec<Out> 
+// where 
+//     F: FnMut(In) -> Out, 
+// {
+//     input.into_iter().map(function).collect()
+// }
+
+
+// Without std.
+pub fn map<F, T, U>(input: Vec<T>, mut function: F) -> Vec<U> 
+where 
+    F: FnMut(T) -> U, 
+{
+    let mut res = Vec::with_capacity(input.len());
+    for x in input.into_iter() {
+        res.push(function(x));
+    }
+    res
 }
