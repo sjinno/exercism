8918c0cc240d5390821e7a195d97f759023ce006
Shohei Jinno <shohei.jinno@gmail.com>
2021-03-07 11:07:11 -0800

    Revise crypto-square solution.

--- crypto-square/src/lib.rs
+++ crypto-square/src/lib.rs
@@ -3,37 +3,22 @@ pub fn encrypt(input: &str) -> String {
         return String::new();
     }
 
-    let mut normalized = input
+    let normalized = input
         .to_ascii_lowercase()
         .chars()
         .filter(|c| c.is_ascii_alphanumeric())
         .collect::<Vec<_>>();
 
-    let length = normalized.len();
-    let rows = (length as f64).sqrt().floor() as usize;
-    let cols;
-    if rows * rows == length {
-        cols = rows;
-    } else {
-        cols = rows + 1;
-    }
+    let sqrt_of_len = (normalized.len() as f64).sqrt();
+    let cols = sqrt_of_len.ceil() as usize;
+    let rows = sqrt_of_len.floor() as usize;
 
-    if rows != cols {
-        for _ in 0..(rows * cols - length) {
-            normalized.push(' ');
-        }
-    }
-
-    let chunks = normalized.chunks(cols).collect::<Vec<_>>();
-    let mut encrypted = String::new();
-    for c in 0..cols {
-        for r in 0..rows {
-            encrypted.push_str(&chunks[r][c].to_string());
-        }
-        if c != cols - 1 {
-            encrypted.push_str(" ");
-        }
-    }
-
-    encrypted
+    (0..cols)
+        .map(|i| {
+            (0..rows)
+                .map(|j| normalized.get(i + j * cols).unwrap_or(&' '))
+                .collect::<String>()
+        })
+        .collect::<Vec<_>>()
+        .join(" ")
 }
