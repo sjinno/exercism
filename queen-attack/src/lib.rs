e714063ad86a22b7b40dc9ac93ad632010148e83
Shohei Jinno <shohei.jinno@gmail.com>
2021-03-27 11:36:32 -0700

    Remove closures.

--- queen-attack/src/lib.rs
+++ queen-attack/src/lib.rs
@@ -9,9 +9,10 @@ pub struct Queen(i32, i32);
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        match (rank, file) {
-            (a, b) if !(0..=7).contains(&a) || !(0..=7).contains(&b) => None,
-            _ => Some(ChessPosition { rank, file }),
+        if !(0..=7).contains(&rank) || !(0..=7).contains(&file) {
+            None
+        } else {
+            Some(ChessPosition { rank, file })
         }
     }
 }
@@ -22,11 +23,8 @@ impl Queen {
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-        let is_same_rank = |other: &Queen| -> bool { self.0 == other.0 };
-        let is_same_file = |other: &Queen| -> bool { self.1 == other.1 };
-        let is_diagonal = |other: &Queen| -> bool {
-            matches!((self.0 - other.0, self.1 - other.1), (a, b) if a.abs() == b.abs())
-        };
-        is_same_rank(other) || is_same_file(other) || is_diagonal(other)
+        self.0 == other.0
+            || self.1 == other.1
+            || matches!((self.0 - other.0, self.1 - other.1), (a, b) if a.abs() == b.abs())
     }
 }
