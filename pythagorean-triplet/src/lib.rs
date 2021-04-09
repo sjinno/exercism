176b7c558afeeadd88faa31beb06857247ca5938
Shohei Jinno <shohei.jinno@gmail.com>
2021-04-09 13:28:10 -0700

    Simply use n * n to square a number.
    I think running the test shows that using `n * n` instead of `n.pow(2)` is a bit faster, like 1 sec on my machine, which feels like quite an improvement actually.

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -2,10 +2,10 @@ use std::collections::HashSet;
 
 pub fn find(sum: u32) -> HashSet<[u32; 3]> {
     let mut hs = HashSet::new();
-    for a in 1..sum / 3 {
-        for b in a..sum / 2 {
+    for a in 2..sum / 3 {
+        for b in a + 1..sum / 2 {
             let c = sum - a - b;
-            if b < c && a.pow(2) + b.pow(2) == c.pow(2) {
+            if b < c && a * a + b * b == c * c {
                 hs.insert([a, b, c]);
             }
         }
