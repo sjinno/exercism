4272a3cc9e7fb9f45dbd763c0bc3f6cacbbdc97f
Shohei Jinno <shohei.jinno@gmail.com>
2021-04-10 18:51:17 -0700

    Use `*` operator instead of `pow`.

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -3,7 +3,7 @@ pub fn square_of_sum(n: u32) -> u32 {
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    (1..=n).fold(0, |acc, x: u32| acc + x.pow(2))
+    (1..=n).fold(0, |acc, x: u32| acc + x * x)
 }
 
 pub fn difference(n: u32) -> u32 {
