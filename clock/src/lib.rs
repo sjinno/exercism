4a774691ce1a6598fc2df7d8d9119d3065fb4f95
Shohei Jinno <shohei.jinno@gmail.com>
2021-04-10 23:26:10 -0700

    clock: Small revision.

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -3,29 +3,29 @@ use std::fmt;
 
 #[derive(Debug, PartialEq)]
 pub struct Clock {
-    hours: Duration,
     mins: Duration,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
         Self {
-            hours: Duration::hours((hours + minutes.div_euclid(60)).rem_euclid(24) as i64),
-            mins: Duration::minutes(minutes.rem_euclid(60) as i64),
+            mins: Duration::minutes(
+                (hours + minutes.div_euclid(60)).rem_euclid(24) as i64 * 60
+                    + minutes.rem_euclid(60) as i64,
+            ),
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
         Self {
-            hours: self.hours + Duration::hours(minutes.div_euclid(60) as i64),
-            mins: self.mins + Duration::minutes(minutes.rem_euclid(60) as i64),
+            mins: self.mins + Duration::minutes(minutes as i64),
         }
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let naive_time = naive::NaiveTime::from_hms(0, 0, 0) + self.hours + self.mins;
+        let naive_time = naive::NaiveTime::from_hms(0, 0, 0) + self.mins;
         write!(f, "{}", naive_time.format("%H:%M"))
     }
 }
