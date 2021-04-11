ac0291e04cc4de04aaf4226ae45840d56ef20eb7
Shohei Jinno <shohei.jinno@gmail.com>
2021-04-10 18:03:32 -0700

    accumulate: Done.

--- accumulate/src/lib.rs
+++ accumulate/src/lib.rs
@@ -1,19 +1,6 @@
-// pub fn map<F, In, Out>(input: Vec<In>, function: F) -> Vec<Out> 
-// where 
-//     F: FnMut(In) -> Out, 
-// {
-//     input.into_iter().map(function).collect()
-// }
-
-
-// Without std.
-pub fn map<F, T, U>(input: Vec<T>, mut function: F) -> Vec<U> 
-where 
-    F: FnMut(T) -> U, 
+pub fn map<F, In, Out>(input: Vec<In>, function: F) -> Vec<Out>
+where
+    F: FnMut(In) -> Out,
 {
-    let mut res = Vec::with_capacity(input.len());
-    for x in input.into_iter() {
-        res.push(function(x));
-    }
-    res
+    input.into_iter().map(function).collect()
 }
