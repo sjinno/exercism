3587be75daf9cdbe942e54429cacfc2f126869f9
Shohei Jinno <shohei.jinno@gmail.com>
2021-04-10 18:08:51 -0700

    prime-factors: Use `if let`.

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,39 +1,13 @@
-// pub fn factors(n: u64) -> Vec<u64> {
-//     if n == 1 {
-//         return vec![];
-//     }
-//     let mut n = n.to_owned();
-//     let mut d = 2;
-//     let mut ans = vec![];
-
-//     loop {
-//         if n.div_euclid(d) == 1 && n.rem_euclid(d) == 0 {
-//             ans.push(d);
-//             break;
-//         }
-//         if n.rem_euclid(d) == 0 {
-//             n /= d;
-//             ans.push(d);
-//             continue;
-//         }
-//         d += 1;
-//     }
-
-//     ans
-// }
-
 pub fn factors(mut n: u64) -> Vec<u64> {
     let mut factors = Vec::new();
     let mut candidates = 2..;
-
-    while n > 1 {
-        let x = candidates.next().unwrap();
-
-        while n % x == 0 {
-            n /= x;
-            factors.push(x);
+    while n != 1 {
+        if let Some(x) = candidates.next() {
+            while n % x == 0 {
+                n /= x;
+                factors.push(x);
+            }
         }
     }
-
     factors
 }
