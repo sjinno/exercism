517801dca8e9dcb0a87585adea0c34a1055fd93f
Shohei Jinno <shohei.jinno@gmail.com>
2021-09-22 23:09:45 -0700

    Finish isbn-verifier exercise.

--- isbn-verifier/src/lib.rs
+++ isbn-verifier/src/lib.rs
@@ -1,9 +1,26 @@
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    let mut filtered_isbn = isbn.chars().filter(|c| c.is_numeric());
-    if filtered_isbn.count() != 10 {
+    if isbn.is_empty() || isbn[..isbn.len() - 1].chars().any(|c| c.is_alphabetic()) {
         return false;
     }
 
-    true
+    let filtered_isbn = isbn
+        .chars()
+        .filter(|c| c.is_numeric() || *c == 'X')
+        .map(|c| {
+            if c == 'X' {
+                return 10;
+            }
+            c.to_digit(10).unwrap()
+        });
+
+    if filtered_isbn.clone().count() != 10 {
+        return false;
+    }
+
+    filtered_isbn
+        .enumerate()
+        .fold(0, |acc, (idx, x)| acc + (10 - idx) * x as usize)
+        .rem_euclid(11)
+        == 0
 }
