f7988c7426d305340b84196024f193108e04f303
Shohei Jinno <shohei.jinno@gmail.com>
2021-09-23 07:25:56 -0700

    Revise isbn-verifier solution.

--- isbn-verifier/src/lib.rs
+++ isbn-verifier/src/lib.rs
@@ -1,26 +1,21 @@
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    if isbn.is_empty() || isbn[..isbn.len() - 1].chars().any(|c| c.is_alphabetic()) {
-        return false;
+    let mut counter = 1;
+    let mut sum = 0;
+
+    for c in isbn.chars().rev() {
+        if c.is_numeric() {
+            sum += c.to_digit(10).unwrap() * counter;
+            counter += 1;
+        } else if c == '-' {
+            continue;
+        } else if counter == 1 && c == 'X' {
+            sum += 10;
+            counter += 1;
+        } else {
+            return false;
+        }
     }
 
-    let filtered_isbn = isbn
-        .chars()
-        .filter(|c| c.is_numeric() || *c == 'X')
-        .map(|c| {
-            if c == 'X' {
-                return 10;
-            }
-            c.to_digit(10).unwrap()
-        });
-
-    if filtered_isbn.clone().count() != 10 {
-        return false;
-    }
-
-    filtered_isbn
-        .enumerate()
-        .fold(0, |acc, (idx, x)| acc + (10 - idx) * x as usize)
-        .rem_euclid(11)
-        == 0
+    counter == 11 && sum.rem_euclid(11) == 0
 }
